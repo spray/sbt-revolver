dd34a21d3197f9ee07e5c662a1f5bbe462453bf0
Johannes Rudolph <johannes.rudolph@gmail.com>
2017-08-15 16:57:39 +0200

    Small style cleanups

--- src/main/scala-sbt-0.13/spray/revolver/SbtCompat.scala
+++ src/main/scala-sbt-0.13/spray/revolver/SbtCompat.scala
@@ -6,12 +6,10 @@ object SbtCompat {
   type Process = sbt.Process
 
   implicit class ForkOptionsCompatOps(val underlying: ForkOptions) extends AnyVal {
-    def withRunJVMOptions(runJVMOptions: Seq[String]): ForkOptions = {
+    def withRunJVMOptions(runJVMOptions: Seq[String]): ForkOptions =
       underlying.copy(runJVMOptions = runJVMOptions)
-    }
 
-    def withOutputStrategy(outputStrategy: sbt.OutputStrategy): ForkOptions = {
+    def withOutputStrategy(outputStrategy: sbt.OutputStrategy): ForkOptions =
       underlying.copy(outputStrategy = Option(outputStrategy))
-    }
   }
 }
