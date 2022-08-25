d3dfc449d5c545dc7f699d332ccda41412b0cba2
tca3 <thomas.cantonnet@gmail.com>
2022-08-25 09:53:54 +0200

    Enable debugging for any address #101

--- src/main/scala/spray/revolver/DebugSettings.scala
+++ src/main/scala/spray/revolver/DebugSettings.scala
@@ -2,7 +2,7 @@ package spray.revolver
 
 case class DebugSettings(port: Int = 5005, suspend: Boolean = false) {
   def toCmdLineArg: String =
-    "-agentlib:jdwp=transport=dt_socket,server=y,suspend=%s,address=%d".format(b2str(suspend), port)
+    "-agentlib:jdwp=transport=dt_socket,server=y,suspend=%s,address=*:%d".format(b2str(suspend), port)
 
   private def b2str(b: Boolean) = if (b) "y" else "n"
 }
