d528e96aa05af34bd95f017fd33660c728006187
tca3 <thomas.cantonnet@gmail.com>
2022-08-25 12:35:04 +0200

    Update DebugSettings.scala

--- src/main/scala/spray/revolver/DebugSettings.scala
+++ src/main/scala/spray/revolver/DebugSettings.scala
@@ -1,8 +1,8 @@
 package spray.revolver
 
-case class DebugSettings(port: Int = 5005, suspend: Boolean = false) {
+case class DebugSettings(port: Int = 5005, suspend: Boolean = false, host: String = "localhost") {
   def toCmdLineArg: String =
-    "-agentlib:jdwp=transport=dt_socket,server=y,suspend=%s,address=*:%d".format(b2str(suspend), port)
+    "-agentlib:jdwp=transport=dt_socket,server=y,suspend=%s,address=%s:%d".format(b2str(suspend), host, port)
 
   private def b2str(b: Boolean) = if (b) "y" else "n"
 }
